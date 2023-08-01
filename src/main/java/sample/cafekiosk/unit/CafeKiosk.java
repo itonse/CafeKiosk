f3de9d46e9e05423fb1c183c24ff7dd256cffda0
itonse <govl3dnjs@gmail.com>
2023-08-01 10:51:38 +0900

    feat : 키오스크에서 음료 담기 기능 추가
    CafeKiosk 클래스의 add 함수를 통해 주문담기 기능 구현
    calculateTotalPrice 함수를 통해 음료값 합계 계산

---
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -0,0 +1,21 @@
+package sample.cafekiosk.unit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CafeKiosk {
+
+    private final List<Beverage> beverages = new ArrayList<>();
+
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+    public int calculateTotalPrice() {
+        int totalPrice = 0;
+        for (Beverage beverage : beverages) {
+            totalPrice += beverage.getPrice();    // 장바구니에 담은 모든 음료 값 합계
+        }
+        return totalPrice;
+    }
+}
---
+++ src/main/java/sample/cafekiosk/unit/CafeKioskRunner.java
@@ -0,0 +1,16 @@
+package sample.cafekiosk.unit;
+
+public class CafeKioskRunner {     // 메인함수가 있는 클래스
+
+    public static void main(String[] args) {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+        System.out.println(">>> 아메리카노 추가");
+
+        cafeKiosk.add(new Latte());
+        System.out.println(">>> 라떼 추가");
+
+        int totalPrice = cafeKiosk.calculateTotalPrice();
+        System.out.println("총 주문가격 : " + totalPrice);
+    }
+}
