7ec5d2031af41233046bee1055c1924749d96716
itonse <govl3dnjs@gmail.com>
2023-08-02 15:21:50 +0900

    test : TDD 3단계 - REFACTOR 2
    더 좋은 코드로 과감하게 리팩토링 하기

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -39,11 +39,9 @@ public class CafeKiosk {
     }
 
     public int calculateTotalPrice() {
-        int totalPrice = 0;
-        for (Beverage beverage : beverages) {
-            totalPrice += beverage.getPrice();    // 장바구니에 담은 모든 음료 값 합계
-        }
-        return totalPrice;
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
 
     public Order createOrder(LocalDateTime currentDateTime) {
